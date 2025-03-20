11a9f0d71e3054c6aab38c6f73335b6661a8dfec
Ayobamiwrld <107368324+Ayobamiwrld@users.noreply.github.com>
2025-03-20 11:38:21 +0000

    in progress

--- weather.js
+++ weather.js
@@ -198,9 +198,15 @@ const updateWeatherUI = (data) => {
         weatherIcon.textContent = 'water_drop';
     }
     else if(description === "Mist" || description === "Fog") {
-        image.src = "DEv image/4651633-hd_1920_1080_30fps.mp4";
-        weatherIcon.className = 'material-symbols-outlined';
-        weatherIcon.textContent = 'mist';
+         // Use a video element for displaying the fog video
+    const videoElement = document.createElement('video');
+    videoElement.src = "DEv image/4651633-hd_1920_1080_30fps.mp4";
+    videoElement.autoplay = true;
+    videoElement.loop = true;
+    videoElement.muted = true;
+    image.replaceWith(videoElement);
+    weatherIcon.className = 'material-symbols-outlined';
+    weatherIcon.textContent = 'mist';
     }
     else {
         image.src = "DEv image/sky-4583_512.gif";
